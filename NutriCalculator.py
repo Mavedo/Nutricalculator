0db085a2b123cad6bf337abf06425585b1a5e358
mavedo <m.vera.d@outlook.com>
2024-04-16 19:44:34 -0600

    .gitignore modified, and Nutricalculator updated

--- NutriCalculator.py
+++ NutriCalculator.py
@@ -8,21 +8,22 @@ class NutriCalculatorApp:
         self.root = root
         self.root.title("Nutri Calculadora")
 
-        # Variables
         self.name_var = tk.StringVar()
         self.age_var = tk.StringVar()
         self.height_var = tk.StringVar()
         self.weight_var = tk.StringVar()
         self.gender_var = tk.StringVar(value="Mujer")  # Default to Female
 
-        # Create UI elements
         self.create_ui()
 
     def create_ui(self):
+        """" Creates the user interface. The labesl and it's entries are distrubuted in 4 rows and 2 columns. 
+        Then 2 radiobuttons are created for the sex category.
+        At the end there's a button that takes all the inputs and calculates."""
         main_frame = ttk.Frame(self.root, padding=(30, 15))
         main_frame.grid(row=0, column=0, sticky="nsew")
         
-        #CREATE THE BUTTONS
+
         ttk.Label(main_frame, text= "Nombre del paciente:").grid(column=0, row=0, sticky="w", pady=5)
         ttk.Entry(main_frame, textvariable= self.name_var).grid(column=1, row=0, sticky="ew", pady=5)
         ttk.Label(main_frame, text= "Edad del paciente:").grid(column=0, row=1, sticky="w", pady=5)
@@ -32,30 +33,32 @@ class NutriCalculatorApp:
         ttk.Label(main_frame, text= "Peso en kilogramos:").grid(column=0, row=3, sticky="w", pady=5)
         ttk.Entry(main_frame, textvariable= self.weight_var).grid(column=1, row=3, sticky="ew", pady=5)
 
-        # Radio Buttons
+
         ttk.Label(main_frame, text="Sexo:").grid(column=0, row=4, sticky="w")
         ttk.Radiobutton(main_frame, text="Hombre", variable=self.gender_var, value="Hombre").grid(column=1, row=4, sticky="w")
         ttk.Radiobutton(main_frame, text="Mujer", variable=self.gender_var, value="Mujer").grid(column=1, row=5, sticky="w")
 
-        # Calculate Button
+
         ttk.Button(main_frame, text="Calcular", command=self.calculate).grid(column=0, row=6, columnspan=2, sticky="ew")
 
     def calculate(self):
+        """" The entries are converted to the right datatype, else an error is returned.
+        We calculate the IMC, the calories according the 2 different equations: Harris-Benedict and
+        Mifflin St. Jeor.
+        
+        The result is returned in a messagebox, a pdf is generated and opened."""
         try:
             age = int(self.age_var.get())
             height = float(self.height_var.get())
             weight = float(self.weight_var.get())
 
-            # Validate inputs
             self.validate_inputs(age, height, weight)
 
-            # Perform calculations
             hb_result = round(self.harris_benedict(weight, height, age),2)
             mf_result = round(self.mifflin(weight, height, age),2)
             imc_result = round(self.imc(weight, height),2)
             classification_result = self.classification_imc(imc_result)
 
-            # Display results
             result_message = f"Harris Benedict: {hb_result}\n" \
                              f"Mifflin St. Jeor: {mf_result}\n" \
                              f"IMC: {imc_result}\n" \
@@ -63,7 +66,6 @@ class NutriCalculatorApp:
 
             messagebox.showinfo("Resultados", result_message)
 
-            # Generate and open PDF
             patient_info = f"Paciente: {self.name_var.get()}, Edad: {age} años, Peso: {weight} kg, Altura: {height} metros."
             self.generate_pdf(hb_result, mf_result, imc_result, classification_result, patient_info)
 
@@ -111,14 +113,14 @@ class NutriCalculatorApp:
 
     def generate_pdf(self, hb_result, mf_result, imc_result, classification_result, patient_info):
         import os
-        pdf_folder = "C:/Users/Faskomilo/Desktop/Nutricalculator/"
+        pdf_folder = "C:/Users/Faskomilo/Desktop/Nutricalculator/patient_results"
         pdf_file = f"{pdf_folder}{self.name_var.get()} resultados.pdf"
 
         if not os.path.exists(pdf_folder):
             os.makedirs(pdf_folder)
 
         c = canvas.Canvas(pdf_file, pagesize=letter)
-        c.drawString(72, 800, "Calculadora NutriMonchi Resultados")
+        c.drawString(72, 800, "Nutri Calculadora Resultados")
         c.drawString(72, 780, patient_info)
         c.drawString(72, 720, f"Harris Benedict: {hb_result} calorías")
         c.drawString(72, 700, f"Mifflin St. Jeor: {mf_result} calorías")
@@ -126,7 +128,6 @@ class NutriCalculatorApp:
         c.drawString(72, 660, f"Clasificación: {classification_result}")
         c.save()
 
-        # Open the PDF file
         os.startfile(pdf_file)
 
 if __name__ == "__main__":
